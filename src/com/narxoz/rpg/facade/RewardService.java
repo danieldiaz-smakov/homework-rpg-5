7c083354ab4379c1a33ae8a87ddc7573d3bb2b1c
Daniel Smakov <danieldiaz.smakov@gmail.com>
2026-03-17 09:10:17 +0500

    Realize RewardService

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,19 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward (invalid result)";
         }
-        return "TODO";
+        if (!battleResult.isHeroWon()) {
+            return "Defeat - no reward";
+        }
+        int rounds = battleResult.getRounds();
+        if (rounds <= 3) {
+            return "Victory reward: Legendary (quick win)";
+        }
+        if (rounds <= 8) {
+            return "Victory reward: Rare";
+        }
+        return "Victory reward: Standard";
     }
 }
